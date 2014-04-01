d9735440a80cc069151fcc72f09170fb651ce007
Mateus L Santos <mateus.lucios@gmail.com>
2014-03-31 22:54:11 -0300

    Initial commit

---
+++ src/com/android/timetracker/TimeTracker.java
@@ -0,0 +1,14 @@
+package com.android.timetracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimeTracker extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+
+}
